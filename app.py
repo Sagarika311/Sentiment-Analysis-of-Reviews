9d9a9b1be2f6461bc731f324e124bdb7fe335522
Sagarika <124549688+Sagarika311@users.noreply.github.com>
2025-09-10 12:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,8 @@ def predict():
         })
 
 if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=PORT, debug=debug)
 
-PORT = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=PORT)
 
